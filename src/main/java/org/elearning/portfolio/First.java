0b2b6c063f1e628f4a44d8025605ba03344614c1
rzonsol <rzonsol@gmail.com>
2016-10-06 21:25:50 +0200

    add spring, problem with mvn clean install

--- src/main/java/org/elearning/portfolio/First.java
+++ src/main/java/org/elearning/portfolio/First.java
@@ -2,14 +2,23 @@ package org.elearning.portfolio;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
  
 public class First {
+
+	private static ApplicationContext context;
 
 	public static void main(String[] args) {
 	    JFrame parent = new JFrame();
 
 	    JOptionPane.showMessageDialog(parent, "Hello World");
+	    
+	    context = new ClassPathXmlApplicationContext(
+				"Spring-Module.xml");
+
+		FirstBean obj = (FirstBean) context.getBean("helloBean");
+		obj.exexute();
 	}
 
 }
---
+++ src/main/java/org/elearning/portfolio/FirstBean.java
@@ -0,0 +1,8 @@
+package org.elearning.portfolio;
+
+public class FirstBean  {
+
+	public void exexute() {
+		System.out.println("Hello World of Spring!");
+	}
+}
