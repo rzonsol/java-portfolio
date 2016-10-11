8e7e2b0cd0b8030db2cba4ff38fa1ab1939cdfe1
rzonsol <rzonsol@gmail.com>
2016-10-11 20:41:50 +0200

    add two module in pom

---
+++ first-step/src/main/java/org/elearning/portfolio/First.java
@@ -0,0 +1,33 @@
+package org.elearning.portfolio;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+public class First {
+
+	private static ApplicationContext context;
+
+	public static void main(String[] args) {
+		context = new ClassPathXmlApplicationContext(
+				"spring-module.xml");
+
+		UserJDBCTemplate userJDBCTemplate =
+				(UserJDBCTemplate)context.getBean("userJDBCTemplate");
+
+
+		System.out.println("------Listing Multiple Records--------" );
+		List<User> users = userJDBCTemplate.listUsers();
+		for (User record : users) {
+			System.out.print("ID : " + record.getId() );
+			System.out.print(", LOGIN : " + record.getLogin() );
+			System.out.print(", FIRST_NAME : " + record.getFirstName());
+			System.out.print(", LAST_NAME : " + record.getLastName());
+			System.out.print(", LOGIN : " + record.getLogin() );
+			System.out.println(", EMAIL : " + record.getEmail());
+		}
+	}
+
+}
