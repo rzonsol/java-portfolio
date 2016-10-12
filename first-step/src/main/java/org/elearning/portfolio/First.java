5548520e5aa472526397fe084992f216013f9770
rzonsol <rzonsol@gmail.com>
2016-10-12 22:40:24 +0200

    add services for messages

--- first-step/src/main/java/org/elearning/portfolio/First.java
+++ first-step/src/main/java/org/elearning/portfolio/First.java
@@ -4,6 +4,7 @@ package org.elearning.portfolio;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.elearning.portfolio.user.*;
+import org.elearning.portfolio.message.*;
 
 import java.util.List;
 
@@ -29,6 +30,57 @@ public class First {
 			System.out.print(", LAST_NAME : " + record.getLastName());
 			System.out.print(", LOGIN : " + record.getLogin() );
 			System.out.println(", EMAIL : " + record.getEmail());
+		}
+
+
+
+		MessageDAOImpl messageDAOImple =
+				(MessageDAOImpl)context.getBean("messageDAOImpl");
+
+		messageDAOImple.createMessage(1,"test","test");
+		messageDAOImple.createMessage(1,"test2","test2");
+		messageDAOImple.createMessage(1,"test3","test3");
+		messageDAOImple.createMessage(1,"test4","test4");
+
+ 		messageDAOImple.createMessage(2,"test","test");
+		messageDAOImple.createMessage(2,"test2","test2");
+		messageDAOImple.createMessage(2,"test3","test3");
+
+ 		messageDAOImple.createMessage(3,"test","test");
+		messageDAOImple.createMessage(3,"test2","test2");
+		messageDAOImple.createMessage(3,"test3","test3");
+
+
+		List<Message> messages = messageDAOImple.listMessage(); //list of all messages
+
+		System.out.println("list of messages");
+		for (Message e : messages){
+			System.out.print("id " + e.getId());
+			System.out.print(", user_id " + e.getUserId());
+			System.out.print(", title " + e.getUserId());
+			System.out.println(", content " + e.getContent());
+		}
+
+		messages = messageDAOImple.userMessages(2); // list of messages belong to user 2
+		System.out.println("list of messages belongs to user id=2");
+		for (Message e : messages){
+			System.out.print("id " + e.getId());
+			System.out.print(", user_id " + e.getUserId());
+			System.out.print(", title " + e.getUserId());
+			System.out.println(", content " + e.getContent());
+		}
+		System.out.println(" the number of messages belong to user id=2 : "+messageDAOImple.countMessages(2)); // count number of messages belong to user 2
+		for(int i=1;i<5;i++){
+			messageDAOImple.delMessage(i);//delate mesage of id i
+		}
+		System.out.println("list of messages after deleted 4 of them");
+
+		messages = messageDAOImple.listMessage(); //list of all messages
+		for (Message e : messages){
+			System.out.print("id " + e.getId());
+			System.out.print(", user_id " + e.getUserId());
+			System.out.print(", title " + e.getUserId());
+			System.out.println(", content " + e.getContent());
 		}
 	}
 
