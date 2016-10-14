d1f774783fedf380769a14c00f8562ca2d4856db
rzonsol <rzonsol@gmail.com>
2016-10-14 09:46:50 +0200

    add service to user

---
+++ first-step/src/main/java/org/elearning/portfolio/services/UserService.java
@@ -0,0 +1,39 @@
+package org.elearning.portfolio.services;
+
+
+import java.util.List;
+import javax.sql.DataSource;
+import org.elearning.portfolio.user.*;
+import org.elearning.portfolio.message.*;
+
+
+public class UserService{
+
+    private UserDAOImpl user;
+
+    public UserDAOImpl getUser(){return user;}
+
+    public void addUser(String login,String email, String firstName, String lastName){
+        user.addUser(login, email, firstName, lastName);
+    }
+
+    public void delUser(Integer id){
+        user.delUser(id);
+    }
+
+    public List<Message> getMessagesByUserId(Integer userId){
+        return user.getMessagesByUserId(userId);
+    }
+
+    public void addUserRole(Integer userId, Integer roleId) {
+        user.addUserRole(userId, roleId);
+    }
+
+    public List<User> getUsers(){
+        return user.getUsers();
+    }
+
+    public List<Role> getUserRoles(Integer userId) {
+        return user.getUserRoles(userId);
+    }
+}
