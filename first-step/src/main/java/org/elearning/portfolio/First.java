6d35c4c7f82854f4e8fb2983c2dfaff23bfea4d2
rzonsol <rzonsol@gmail.com>
2016-10-14 12:35:25 +0200

    problem with userService

--- first-step/src/main/java/org/elearning/portfolio/First.java
+++ first-step/src/main/java/org/elearning/portfolio/First.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.elearning.portfolio.user.*;
 import org.elearning.portfolio.message.*;
+import org.elearning.portfolio.services.UserService;
 
 import java.util.List;
 
@@ -17,9 +18,18 @@ public class First {
 		context = new ClassPathXmlApplicationContext(
 				"spring-module.xml");
 
+		UserService user =
+				(UserService)context.getBean("userService");
 
+			List<User> users = user.getUsers();
 
-// testing data
+		for (User e : users){
+			System.out.print("id " + e.getId());
+			System.out.print(", login " + e.getLogin());
+			System.out.print(", email " + e.getEmail());
+			System.out.print(", firstName " + e.getFirstName());
+			System.out.println(", lastName " + e.getLastName());
+		}
 
 	}
 
