de0080d8eb8c15478c0c5a5b3ad8746e0c82f61f
rzonsol <rzonsol@MacBook-Air-Piotr.local>
2016-10-21 20:54:39 +0200

    change message to messageDao

--- first-step-spring-class-config/src/main/java/org/elearning/portfolio/services/MessageService.java
+++ first-step-spring-class-config/src/main/java/org/elearning/portfolio/services/MessageService.java
@@ -10,29 +10,29 @@ import org.elearning.portfolio.message.*;
 public class MessageService{
 
     @Autowired
-    private MessageDAO message;
+    private MessageDAO messageDao;
 
-    public MessageService(MessageDAO message){
-        this.message= message;
+    public MessageService(MessageDAO messageDao){
+        this.messageDao= messageDao;
     }
 
     public void createMessage(Integer userId, String title, String content){
-        message.createMessage(userId, title, content);
+        messageDao.createMessage(userId, title, content);
     }
 
     public List <Message>  getUserMessages(Integer userId){
-        return message.getUserMessages(userId);
+        return messageDao.getUserMessages(userId);
     }
 
     public void delMessage(Integer id){
-        message.delMessage(id);
+        messageDao.delMessage(id);
     }
 
     public Integer countMessages(Integer userId){
-        return message.getUserMessages(userId).size();
+        return messageDao.getUserMessages(userId).size();
     }
 
     public List<Message> getMessages(){
-        return message.getMessages();
+        return messageDao.getMessages();
     }
 }
